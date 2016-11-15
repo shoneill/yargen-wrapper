93fc4217eaa6f07689bd3161e4d7657a1324968d
zeev0 <zeevozeevo@gmail.com>
2016-11-15 11:29:52 -0500

    Update README.md and INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install scandir lxml naiveBayesClassifier pefile -t
+pip install scandir lxml naiveBayesClassifier pefile -t .
 git clone https://github.com/binarlyhq/binarly-sdk/
 7z x good-strings.db.zip.001
 7z x good-opcodes.db.zip.001
